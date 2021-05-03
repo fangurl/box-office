6c7259cf400c9b3e3e69e00ffbee9fae4860c5e3
fangurl <vinitishrivastava3@gmail.com>
2021-05-03 20:14:18 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      hello!
-    </div>
+    <Switch>
+      <Route exact path="/">
+        Welcome to Home Page
+      </Route>
+    
+    <Route exact path="/starred">
+      Your starred items here
+    </Route>
+
+    <Route> 404: page not found
+    </Route>
+  </Switch>
   );
 }
 
